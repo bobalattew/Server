37f01c8b70f60c0ff9fe4c7e44e80f2cfb23ba14
marykrshaa <48324374+marykrshaa@users.noreply.github.com>
2019-03-19 15:36:23 +0800

    Create server.java

---
+++ server.java
@@ -0,0 +1,18 @@
+Import java.net.*;
+Import java.io.*;
+Import java.lang.*;
+
+class TcpJavaServer{
+
+      public static void main (String args[]) throws IOException
+      {
+      String data="Your Connection has reached us\n";
+      ServerSocket srvr = new ServerSocket(9002);
+      Socket skt = srvr.accept();
+      System.out.printIn("Connection Has Been established from ip address of:" +skt.getInetAddress()+"\n");
+      PrintWriter out = new PrintWriter(skt.getOutputStream(),true);
+      out.print(data);
+      out.close();
+      skt.close();
+      srvr.close();
+      }
