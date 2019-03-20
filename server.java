cc6b0549ec7892004b1e26e7a347456f4d07214a
marykrshaa <48324374+marykrshaa@users.noreply.github.com>
2019-03-20 17:16:14 +0800

    Update server.java

--- server.java
+++ server.java
@@ -11,16 +11,18 @@ class TcpJavaServer{
       ServerSocket srvr = new ServerSocket(9002);
       Socket skt = srvr.accept();
             
+      //sending string
+      String data="Whatsupp?";
+      PrintWriter out = new PrintWriter(skt.getOutputStream(),true);
+      out.print(data);
+            
       //receivng string
       InputStreamReader in = new InputStreamReader(skt.getInputStream());
       BufferedReader bf= new BufferedReader(in);
       String str=bf.readLine();
       System.out.println("Client said: " +str+"\n");
             
-      //sending string
-      String data="Whatsupp?";
-      PrintWriter out = new PrintWriter(skt.getOutputStream(),true);
-      out.print(data);
+      
       
       //closing connection
       out.close();
